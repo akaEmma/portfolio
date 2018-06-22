9e7c6386615e977b6a2b505717f0cb3c6b2ab881
akaEmma <37959858+akaEmma@users.noreply.github.com>
2018-06-22 14:51:45 -0400

    Add some comments
    I also renamed the str_num to n.

--- explore_brain.r
+++ explore_brain.r
@@ -1,33 +1,49 @@
-# functions for my personal use to load, print, and generally explore some brain images.
-# not broken into separate files so it's easier to upload the set.
+# These functions are for my personal use to load, print, and generally explore some brain images. They have not been 
+#       properly generalized, but if I end up using them a lot I will make the necessary changes. 
+# I have not broken the functions into separate files because I wanted to upload them here in a single file for easy reading.
+
+# The data: DICOM image files. Detailed explanations of these data types are provided in the Section 6.2 (part 5) of the DICOM standard
+#       (http://medical.nema.org). 
+# If you want to try this code, download some .dcm image files from https://www.dicomlibrary.com/ and set "pname" to your data dir.
+# You will need to make sure your naming conventions are the same as mine, because, as I said, this code is not generalized for
+#       anybody's use but mine. 
+
+#Packages and libraries
 
 require("oro.dicom")
 require("oro.nifti")
 library(oro.nifti)
 library(oro.dicom)
 
-pname = "your pname here"  # will be needed in most of these functions.
+pname = "your pname here"  # will be needed in most of these functions. In my personal copy I have set it to be where I keep the .dcm data files. 
 
 #Read in an image that represents a single slice of a brain.
-#It assumes a specific file naming convention and it assumes that the file is of DICOM format.
+#This function assumes a specific file naming convention common to .dcm files and it assumes that the file is of DICOM format.
+#       "pname" is the pathname to the .dcm files. 
+#       "n" is the number of the dcm file. For instance, these files often have names like "IM-001-0011.dcm" or "IM-001-0009.dcm". 
+#               If you pass n = 11 to this function, it will look for "IM-001-0011.dcm". 
+#               If you pass it n = 9, it will look for "IM-001-0009.dcm".
+#               "n" can be either numeric or string.
+#               
 
-get_dslice <- function(pname, string_num) {
-        fname = paste("IM-0001-00", string_num, ".dcm", sep = "")
+get_dslice <- function(pname, n) {
+        fname = paste("IM-0001-00", n, ".dcm", sep = "")
         pfname = (paste(pname, fname, sep = ""))
         myslice= readDICOM(pfname)
         return(myslice)
 }
 
 #get a slice into memory
+#first check if it is already in memory to save loading time if possible. 
 if(!exists("myslice")) {
         myslice = get_dslice(pname, 11)
 }
 
-#look at it
+#look at the slice
 class(myslice)
 names(myslice)
 
-# Looking more deeply into the file structure:
+# Look more deeply into the file structure:
 
 str(myslice)
 class(myslice$hdr[[1]])
@@ -41,9 +57,10 @@ str(myslice$hdr[[1]])
 #Steps used to see the image include transposing it (because it is a matrix) and using the image() function. 
 
 #Parameters:    
-#        - "1:d[1]" means take all the rows    
+#       - "1:d[1]" means take all the rows    
 #       - "1:d[2]" means take ll the columns    
 #       - t() transposes the image. 
+# Pass myslice, which is what is returned by the get_dslice() function above.
 
 prt_dslice <- function(myslice) {
         d = dim(t(myslice$img[[1]]))
@@ -55,7 +72,7 @@ prt_dslice(myslice)
 
 # This function reads multiple slice files from the input pathname.
 # range beginning and end may be numbers or strings.
-# Assumes that file names follow a specific naming convenstion, as shown in the function get_dslice() above.
+# This function assumes that file names follow the naming convention as shown in the function get_dslice() above.
 # There are only 22 images in my directory, and that will commonly be the case for other people because
 #       it is currently the convention to have 22 MRI DICOM images. 
 
