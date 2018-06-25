efac3a88221339ff0695df1e083a56a9b8e8aa76
akaEmma <37959858+akaEmma@users.noreply.github.com>
2018-06-25 17:41:50 -0400

    Coding sample in R
    Extracts an unstable but relevant set of vars by excluding a stable set of unwanted vars. We never know which variables we'll be passed, but we do know which variables we don't need in the data set. This function uses logic based on that fact and it works because NOT finding a var in a dataset kicks off a warning, but not FINDING a var in a data set causes a program-stopping error. We want warnings, not errors, in this situation.

---
+++ get_interesting_vars_a.r
@@ -0,0 +1,58 @@
+#########################################
+#                                       
+#       get_interesting_vars()          
+#                                       
+# tib = this project's tibble or data frame 
+# irrelevant_v = vector of vars we don't need 
+#
+#-------------------------------------- 
+# Add items to the irrelevant_V on a   
+#   project-to-project basis. No need   
+#   to delete any names.       
+#########################################
+
+get_interesting_vars <- function(tib, irrelevant_v) {
+
+        #get a list of names excluding any member of the irrelevant set,
+        #then subset tib by all except those names
+        
+        names.use <- names(tib)[!(names(tib) %in% irrelevant_v)]
+        tib <- b[, names.use]
+        
+        # Now exclude any variable that begins with "x".
+        # In this dataset, any such variable is an empty column
+        # usually passed in from upstream at the end of the Excel csv.
+        #
+        # Using "-starts_with" returns all that don't start with x.
+        #       None of our relevant vars begins with x. If that changes,
+        #       this way of deleting the Xs will cause problems.
+        #       However, it is still the best way because we don't know
+        #       what the x's end with. They end with numbers, but we won't
+        #       know what they are unless we look. This method is therefore
+        #       the best (despite being imperfect) way to keep the code 
+        #       generalizable.
+        
+        tib %>% dplyr:: select(-starts_with("x"))
+}
+
+irrelevant_v <- c("startdate", 
+                    "enddate", 
+                    "status", 
+                    "ipaddress",
+                    "progress", 
+                    "duration (in seconds)",
+                    "duration..in.seconds.",
+                    "finished",
+                    "recordeddate",
+                    "responseid", 
+                    "recipientlastname",
+                    "recipientfirstname", 
+                    "recipientemail",
+                    "externalreference", 
+                    "locationlatitude",
+                    "locationlongitude", 
+                    "distributionchannel",
+                    "userlanguage",
+                    "name")
+
+get_interesting_vars(tib, irrelevant_v)
